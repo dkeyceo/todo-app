cc365ee4d10fb635f2516da8fdf4e215f8cc32cd
dkeyceo <dkeyceo@gmail.com>
2024-03-07 18:21:15 +0200

    add sqlx, described init of dbase layer, described configs, extract password to env

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/subosito/gotenv"
 
 	"github.com/dkeyceo/todo-app"
 	"github.com/dkeyceo/todo-app/pkg/handler"
@@ -15,7 +19,24 @@ func main() {
 		log.Fatalf("error initialization config: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := gotenv.Load(); err != nil {
+		log.Fatalf("error loading env vars: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("error during init db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
